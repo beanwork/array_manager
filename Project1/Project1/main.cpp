684a562df25d2d54cedd1ed8bd2f7024a6d67f4e
user <user@>
2024-06-28 15:25:50 +0900

    다시 1차원 배열 부터 완성

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -3,19 +3,29 @@
 
 using namespace std;
 
+void Make1dimArray();
 void Make2dimArray(int** array_2, int num);
 void InputRandNumber(int* array, int num);
 
 
+void Make1dimArray()
+{
+	int column_num;
+
+	cout << endl;
+	cout << "select column number" << endl;
+	cin >> column_num;
+
+	int *array_1 = new int[column_num]; // 1 dimension array dynamic allocation 
+	InputRandNumber(array_1, column_num);
+
+	delete[] array_1;
+}
+
+
 void Make2dimArray(int** array_2, int num)
 {
-	*array_2 = new int[num];
-	for (int i = 0; i < num; i++)
-	{
-		InputRandNumber(*array_2, num);
-		*array_2++;
-	}
-	delete[] array_2;
+
 }
 
 
@@ -44,23 +54,14 @@ void main()
 	cout << "select array dimension" << endl;
 	cin >> num;
 
-	int *array_1 = new int[num]; // 1 dimension array dynamic allocation 
-
 	switch (num)
 	{
 	case 1:
-		break;
-	case 2:
-		Make2dimArray(&array_1, num);
-		break;
-	case 3:
-		Make2dimArray(&array_1, num);
-		break;
+		Make1dimArray();
 	default:
-		if (num > 3) cout << "유효하지 않은 접근입니다" << endl;
 		break;
 	}
-	InputRandNumber(array_1, num);
-	delete[] array_1; 
+	
+
 
 }
